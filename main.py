55c89d2d1f2aac92b8b9a6812de0f3344d8c18fc
22pwbcs0901@uetpeshawar.edu.pk <webdevolopmentbyar@gmail.com>
2025-03-22 09:33:15 +0500

    SETUP:Adding configuration files and server setup

--- main.py
+++ main.py
@@ -11,28 +11,7 @@ def mine_block():
 def is_server_running():
     return 'Server is Running Successfully', 200
 
-# Vercel serverless function handler
-def handler(event, context):
-    from flask import Request
-
-    # Create a fake request object from the event
-    with app.test_request_context(
-        path=event['path'],
-        method=event['httpMethod'],
-        headers=event.get('headers', {}),
-        data=event.get('body', '')
-    ):
-        # Dispatch the request to the Flask app
-        response = app.full_dispatch_request()
-
-        # Return the response in the format Vercel expects
-        return {
-            'statusCode': response.status_code,
-            'headers': dict(response.headers),
-            'body': response.data.decode('utf-8')
-        }
 
 # Run the app locally if not on Vercel
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
